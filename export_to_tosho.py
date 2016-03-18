5b5c2db7f0423b10fb0120dc4c1558455bed1a0b
Momchil Tomov <momchil@memsql.com>
2016-03-18 16:47:36 -0700

    moved solution to subdir
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Maniphest Tasks:

---
+++ export_to_tosho.py
@@ -0,0 +1,19 @@
+from solution import *
+
+from test import ASA_Sandstone_1 as target
+
+if __name__ == '__main__':
+
+    solution = Solution(target, [0, 1], [0, 1], [0, 1])
+    solution.deserialize("best_greedy_1.json")
+
+    print 'cost = ', solution.cost
+    print 'hist = ', solution.hist
+    print 'target = ', solution.target
+
+    solution.exportForTosho()
+
+    sanity = Solution(solution.target, solution.x_range, solution.y_range, solution.z_range)
+    sanity.importFromTosho()
+
+    assert sanity.isEqual(solution)
