fdf8c2c9a41a8cfd741f88ecf46b6908a0327592
Momchil Tomov <momchil@memsql.com>
2016-03-18 20:51:52 -0700

    cleanup the code
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Maniphest Tasks:

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from __future__ import division
 
 from solution import *
 from genetic import *
-from greedy import *
+#from greedy_first_better_neighbor import *
+from greedy_best_neighbor import *
 
 from multiprocessing import Process
 
@@ -10,9 +11,9 @@ from multiprocessing import Process
 # and that center points are at odd coordinates
 # see https://en.wikipedia.org/wiki/Cubic_crystal_system
 
-x_range = [0, 100]
-y_range = [0, 100]
-z_range = [0, 100]
+x_range = [0, 10]
+y_range = [0, 10]
+z_range = [0, 10]
 
 ASA_Sandstone_1 = [0.1590810577, 0.1239705245, 0.1352405722, 0.1404421326, 0.1005635024, 0.07195491981, 0.05938448201, 0.0455136541, 0.04421326398, 0.03077589944, 0.02080624187, 0.01517121803, 0.0125704378, 0.00736887733, 0.00736887733, 0.004768097096, 0.004768097096, 0.002600780234, 0.003034243606, 0.001733853489, 0.002167316862, 0.001300390117, 0.0008669267447, 0.0004334633723, 0.0008669267447, 0.001300390117, 0.0004334633723, 0.0004334633723, 0, 0, 0, 0, 0, 0.0004334633723, 0, 0, 0, 0, 0.0004334633723]
 
@@ -27,32 +28,9 @@ def infinite_greedy(thd):
         break
 
 if __name__ == '__main__':
-    print getN([0, 15 * 2], [0, 15 * 2], [0, 15 * 2])
-
-    adj = getAllAdj((1, 1, 1))
-    for point in adj:
-        print point
-    print range(14)
-    print revAdjIdx
-
     print truncate(ASA_Sandstone_1)
     target = truncateAndScale(ASA_Sandstone_1, x_range, y_range, z_range)
     print target
-
-    print getN(x_range, y_range, z_range)
-    solution = Solution(target, x_range, y_range, z_range, randomize=True, prob = 0.5)
-    #print len(solution.throats)
-    #solution.output()
-
-    solution.serialize('wtf.json')
-
-    test_solution = Solution(target, [0, 1], [0, 1], [0, 1])
-    test_solution.deserialize('wtf.json')
-    
-    assert solution.isEqual(test_solution)
-
-    #for point, idxs in throatIdxs.iteritems():
-    #    print point, ' -> ', idxs
 
     procs = [Process(target=infinite_greedy, args=(i,)) for i in range(1)]
     [p.start() for p in procs]
---
+++ sanity.py
@@ -0,0 +1,44 @@
+from __future__ import division
+
+from solution import *
+
+
+# assumes the corner points are at even coordinates
+# and that center points are at odd coordinates
+# see https://en.wikipedia.org/wiki/Cubic_crystal_system
+
+x_range = [0, 10]
+y_range = [0, 10]
+z_range = [0, 10]
+
+ASA_Sandstone_1 = [0.1590810577, 0.1239705245, 0.1352405722, 0.1404421326, 0.1005635024, 0.07195491981, 0.05938448201, 0.0455136541, 0.04421326398, 0.03077589944, 0.02080624187, 0.01517121803, 0.0125704378, 0.00736887733, 0.00736887733, 0.004768097096, 0.004768097096, 0.002600780234, 0.003034243606, 0.001733853489, 0.002167316862, 0.001300390117, 0.0008669267447, 0.0004334633723, 0.0008669267447, 0.001300390117, 0.0004334633723, 0.0004334633723, 0, 0, 0, 0, 0, 0.0004334633723, 0, 0, 0, 0, 0.0004334633723]
+
+if __name__ == '__main__':
+    print getN([0, 15 * 2], [0, 15 * 2], [0, 15 * 2])
+
+    adj = getAllAdj((1, 1, 1))
+    for point in adj:
+        print point
+    print range(14)
+    print revAdjIdx
+
+    print truncate(ASA_Sandstone_1)
+    target = truncateAndScale(ASA_Sandstone_1, x_range, y_range, z_range)
+    print target
+
+    print getN(x_range, y_range, z_range)
+    solution = Solution(target, x_range, y_range, z_range, randomize=True, prob = 0.5)
+    #print len(solution.throats)
+    #solution.output()
+
+    solution.serialize('wtf.json')
+
+    test_solution = Solution(target, [0, 1], [0, 1], [0, 1])
+    test_solution.deserialize('wtf.json')
+    
+    assert solution.isEqual(test_solution)
+
+    #for point, idxs in throatIdxs.iteritems():
+    #    print point, ' -> ', idxs
+
+    # TODO test costIfSet and setAndRecalc...
